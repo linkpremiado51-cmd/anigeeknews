4a3a4f0dfe84c1a07008d6ee7647796dc37ae927
linkpremiado51-cmd <linkpremiado51@gmail.com>
2025-12-19 10:23:07 -0300

    Create dados-manchetes.js

---
+++ dados_de_noticias/dados-manchetes.js
@@ -0,0 +1,18 @@
+export const dadosManchetes = [
+    {
+        img: "https://images.unsplash.com/photo-1542751371-adc38448a05e?w=800",
+        categoria: "Destaque",
+        titulo: "Novas produções originais são anunciadas para 2026",
+        descricao: "O mercado de animes deve receber um investimento recorde no próximo ano, com foco em adaptações de Manhwas.",
+        meta: "há 1 hora • Leitura: 5min",
+        likes: 120
+    },
+    {
+        img: "https://i.postimg.cc/sfHMcTDy/58awkrh8lp404dsegeqpbffcz.jpg",
+        categoria: "Mercado",
+        titulo: "Sony confirma evento focado em hardware para o próximo mês",
+        descricao: "Rumores indicam que o sucessor do PlayStation Portal será o grande protagonista da apresentação.",
+        meta: "há 3 horas • Leitura: 4min",
+        likes: 85
+    }
+];
