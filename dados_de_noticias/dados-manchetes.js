3fce22eb8253bf0e280ef628dc08dbd33b83fe76
linkpremiado51-cmd <linkpremiado51@gmail.com>
2025-12-25 22:31:59 -0300

    Update dados-manchetes.js

--- dados_de_noticias/dados-manchetes.js
+++ dados_de_noticias/dados-manchetes.js
@@ -5,7 +5,8 @@ export const dadosManchetes = [
         titulo: "Novas produções originais são anunciadas para 2026",
         descricao: "O mercado de animes deve receber um investimento recorde no próximo ano, com foco em adaptações de Manhwas.",
         meta: "há 1 hora • Leitura: 5min",
-        likes: 120
+        likes: 120,
+        url: "https://linkpremiado51-cmd.github.io/anigeeknews/notícias/animes/2026/2026-o-ano-dos-retornos-epicos.html"
     },
     {
         img: "https://i.postimg.cc/sfHMcTDy/58awkrh8lp404dsegeqpbffcz.jpg",
@@ -13,7 +14,8 @@ export const dadosManchetes = [
         titulo: "Sony confirma evento focado em hardware para o próximo mês",
         descricao: "Rumores indicam que o sucessor do PlayStation Portal será o grande protagonista da apresentação.",
         meta: "há 3 horas • Leitura: 4min",
-        likes: 85
+        likes: 85,
+        url: "https://linkpremiado51-cmd.github.io/anigeeknews/notícias/animes/2026/2026-o-ano-dos-retornos-epicos.html"
     },
     {
         img: "https://i.postimg.cc/sfHMcTDy/58awkrh8lp404dsegeqpbffcz.jpg",
@@ -21,7 +23,8 @@ export const dadosManchetes = [
         titulo: "Vazamento detalha especificações do suposto 'PS Portal 2'",
         descricao: "Fontes ligadas à cadeia de suprimentos indicam uso de telas OLED Samsung e integração nativa com nuvem.",
         meta: "há 2 horas • Leitura: 3min",
-        likes: 0
+        likes: 0,
+        url: "https://linkpremiado51-cmd.github.io/anigeeknews/notícias/hardware/ps-portal-2-vazamento.html"
     },
     {
         img: "https://i.postimg.cc/QdzLYWKg/Jujutsu-Kaisen01.jpg",
@@ -29,6 +32,7 @@ export const dadosManchetes = [
         titulo: "Jujutsu Kaisen: Gege Akutami anuncia pausa por saúde",
         descricao: "Editorial da Jump confirma hiato de duas semanas. Fãs debatem condições de trabalho na indústria.",
         meta: "há 4 horas • Leitura: 2min",
-        likes: 0
+        likes: 0,
+        url: "https://linkpremiado51-cmd.github.io/anigeeknews/notícias/mangas/jujutsu-kaisen-pausa.html"
     }
 ];
