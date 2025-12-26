606db219512ac6414df2d4dff2fc7443e3ec2457
linkpremiado51-cmd <linkpremiado51@gmail.com>
2025-12-25 21:37:50 -0300

    Update dados-manchetes.js

--- dados_de_noticias/dados-manchetes.js
+++ dados_de_noticias/dados-manchetes.js
@@ -14,5 +14,21 @@ export const dadosManchetes = [
         descricao: "Rumores indicam que o sucessor do PlayStation Portal será o grande protagonista da apresentação.",
         meta: "há 3 horas • Leitura: 4min",
         likes: 85
+    },
+    {
+        img: "https://i.postimg.cc/sfHMcTDy/58awkrh8lp404dsegeqpbffcz.jpg",
+        categoria: "Hardware",
+        titulo: "Vazamento detalha especificações do suposto 'PS Portal 2'",
+        descricao: "Fontes ligadas à cadeia de suprimentos indicam uso de telas OLED Samsung e integração nativa com nuvem.",
+        meta: "há 2 horas • Leitura: 3min",
+        likes: 0
+    },
+    {
+        img: "https://i.postimg.cc/QdzLYWKg/Jujutsu-Kaisen01.jpg",
+        categoria: "Manga",
+        titulo: "Jujutsu Kaisen: Gege Akutami anuncia pausa por saúde",
+        descricao: "Editorial da Jump confirma hiato de duas semanas. Fãs debatem condições de trabalho na indústria.",
+        meta: "há 4 horas • Leitura: 2min",
+        likes: 0
     }
 ];
