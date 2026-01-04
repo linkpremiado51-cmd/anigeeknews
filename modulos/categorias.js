db757629f96e1efe6b7aa68ff419ec0ff787dde6
linkpremiado51-cmd <linkpremiado51@gmail.com>
2026-01-04 15:44:05 -0300

    Create categorias.js

---
+++ modulos/categorias.js
@@ -0,0 +1,94 @@
+// Arquivo: /anigeeknews/modulos/categorias.js
+
+export const categorias = {
+  Tecnologia: [
+    "Hardware",
+    "Consoles",
+    "Streaming",
+    "Mobile",
+    "Inteligência Artificial",
+    "Cloud",
+    "Redes",
+    "Segurança",
+    "Realidade Virtual",
+    "Wearables"
+  ],
+
+  Animes: [
+    "Shonen",
+    "Seinen",
+    "Shojo",
+    "Isekai",
+    "Mecha",
+    "Slice of Life",
+    "Romance",
+    "Ação",
+    "Fantasia",
+    "Comédia"
+  ],
+
+  Games: [
+    "RPG",
+    "Ação",
+    "FPS",
+    "Indie",
+    "Mobile",
+    "MMO",
+    "Esportes",
+    "Estratégia",
+    "Survival",
+    "Retro"
+  ],
+
+  Mangás: [
+    "Shonen",
+    "Seinen",
+    "Shojo",
+    "Webtoon",
+    "One-shot",
+    "Light Novel",
+    "Manhwa",
+    "Manhua",
+    "Slice of Life",
+    "Fantasia"
+  ],
+
+  Filmes: [
+    "Ação",
+    "Drama",
+    "Ficção Científica",
+    "Terror",
+    "Suspense",
+    "Comédia",
+    "Animação",
+    "Fantasia",
+    "Aventura",
+    "Documentário"
+  ],
+
+  Séries: [
+    "Drama",
+    "Ação",
+    "Sci-Fi",
+    "Fantasia",
+    "Comédia",
+    "Suspense",
+    "Crime",
+    "Animação",
+    "Reality",
+    "Documentário"
+  ],
+
+  Cinema: [
+    "Bilheteria",
+    "Críticas",
+    "Lançamentos",
+    "Premiações",
+    "Festivais",
+    "Diretores",
+    "Roteiros",
+    "Trilhas Sonoras",
+    "Análises",
+    "Curiosidades"
+  ]
+};
