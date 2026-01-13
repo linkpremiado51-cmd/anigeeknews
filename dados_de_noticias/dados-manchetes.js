4be2d6bfdb32c698e1358b8961eb46869109818b
linkpremiado51-cmd <linkpremiado51@gmail.com>
2026-01-13 13:05:02 -0300

    Update dados-manchetes.js

--- dados_de_noticias/dados-manchetes.js
+++ dados_de_noticias/dados-manchetes.js
@@ -1,15 +1,1 @@
-// dados_de_noticias/dados-manchetes.js
 
-export const dadosManchetes = [
-    
-    {
-        img: "https://i.ibb.co/CK7yQXT6/header-2.jpg",
-        categoria: "Games",
-        subcategoria: "Mobile",
-        titulo: "Solo Leveling: ARISE ganha grande atualização de conteúdo",
-        descricao: "A Netmarble lançou a primeira atualização do ano para Solo Leveling: ARISE, com raid cooperativa e eventos.",
-        meta: "17 Jan, 2025 • Atualização",
-        likes: 0,
-        url: "https://linkpremiado51-cmd.github.io/anigeeknews/not%C3%ADcias/games/solo-leveling-arise-grande-atualizacao.html"
-    }
-];
